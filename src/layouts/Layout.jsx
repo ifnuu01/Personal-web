4fcadfe8832dc13bfd4e1a8f1299cdd543508463
ifnuu01 <ifnuu01@gmail.com>
2025-03-02 21:54:30 +0800

    style : bug background

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -5,7 +5,7 @@ import Footer from "../components/Footer";
 function Layout({ children }) {
   return (
     <>
-      <div className="flex flex-col bg-custom-bg bg-cover bg-center h-full">
+      <div className="flex flex-col min-h-screen bg-custom-bg bg-cover bg-center bg-fixed">
         <Header />
         {children}
         <Footer />
