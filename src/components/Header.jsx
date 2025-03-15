fae3874073f80bcaa49042a6ec3b4d42185de3a2
ifnuu01 <ifnuu01@gmail.com>
2025-03-15 17:15:28 +0800

    style : memperbaiki style navbar di mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   const location = useLocation();
   return (
     <>
-      <div className="relative bg-white rounded-3xl shadow-lg p-6 w-[90%] lg:w-[40%] text-center mx-auto mt-20">
+      <div className="relative bg-white rounded-3xl shadow-lg p-6 w-[100%] lg:w-[40%] text-center mx-auto mt-20 mb-4">
         <div className="absolute -top-14 left-1/2 transform -translate-x-1/2 w-28 h-28 rounded-2xl z-10 flex justify-center items-center">
           <img src={foto} alt="" className="rounded-2xl shadow-2xl" />
         </div>
@@ -77,56 +77,55 @@ const Header = () => {
             />
           </motion.a>
         </div>
-
-        <div className="flex justify-between lg:justify-evenly mt-6 text-primary font-semibold">
-          {/* About */}
-          <Link
-            to="/"
-            className="relative flex items-center cursor-pointer group"
-          >
-            <span className="text-md lg:text-lg">👋 About</span>
-            <span
-              className={`absolute bottom-0 top-6 lg:top-8 left-0 w-full h-[2px] bg-[#FC882F] 
+      </div>
+      <div className="flex justify-between lg:justify-evenly text-primary font-semibold bg-white py-4 px-2 lg:px-2 shadow-lg w-[100%] lg:w-[40%] mx-auto fixed bottom-0 left-0 right-0 z-10 backdrop-blur bg-opacity-60 lg:sticky lg:top-5 lg:py-4 lg:rounded-3xl lg:shadow-2xl">
+        {/* About */}
+        <Link
+          to="/"
+          className="relative flex items-center cursor-pointer group"
+        >
+          <span className="text-md lg:text-lg">👋 About</span>
+          <span
+            className={`absolute bottom-0 top-6 lg:top-8 left-0 w-full h-[2px] bg-[#FC882F] 
                 ${
                   location.pathname === "/"
                     ? "scale-x-100"
                     : "scale-x-0 group-hover:scale-x-100"
                 } transition-transform duration-300 ease-in-out`}
-            ></span>
-          </Link>
+          ></span>
+        </Link>
 
-          {/* Projects */}
-          <Link
-            to="/projects"
-            className="relative flex items-center cursor-pointer group"
-          >
-            <span className="text-md lg:text-lg">🛠️ Projects</span>
-            <span
-              className={`absolute bottom-0 top-6 lg:top-8 left-0 w-full h-[2px] bg-[#FC882F] 
+        {/* Projects */}
+        <Link
+          to="/projects"
+          className="relative flex items-center cursor-pointer group"
+        >
+          <span className="text-md lg:text-lg">🛠️ Projects</span>
+          <span
+            className={`absolute bottom-0 top-6 lg:top-8 left-0 w-full h-[2px] bg-[#FC882F] 
                 ${
                   location.pathname === "/projects"
                     ? "scale-x-100"
                     : "scale-x-0 group-hover:scale-x-100"
                 } transition-transform duration-300 ease-in-out`}
-            ></span>
-          </Link>
+          ></span>
+        </Link>
 
-          {/* Resume */}
-          <Link
-            to="/resume"
-            className="relative flex items-center cursor-pointer group"
-          >
-            <span className="text-md lg:text-lg">📄 Resume</span>
-            <span
-              className={`absolute bottom-0 top-6 lg:top-8 left-0 w-full h-[2px] bg-[#FC882F] 
+        {/* Resume */}
+        <Link
+          to="/resume"
+          className="relative flex items-center cursor-pointer group"
+        >
+          <span className="text-md lg:text-lg">📄 Resume</span>
+          <span
+            className={`absolute bottom-0 top-6 lg:top-8 left-0 w-full h-[2px] bg-[#FC882F] 
                 ${
                   location.pathname === "/resume"
                     ? "scale-x-100"
                     : "scale-x-0 group-hover:scale-x-100"
                 } transition-transform duration-300 ease-in-out`}
-            ></span>
-          </Link>
-        </div>
+          ></span>
+        </Link>
       </div>
     </>
   );
