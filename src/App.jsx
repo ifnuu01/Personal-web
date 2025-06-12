36f2c7e28bd24b2a863407c8c3af85ade8612449
ifnuu01 <ifnuu01@gmail.com>
2025-06-12 13:39:19 +0800

    feat : menambahkan route detail

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Projects from "./layouts/Projects";
 import BlogPage from "./layouts/BlogPage";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import BlogDetail from "./components/BlogDetail";
 
 function App() {
   useEffect(() => {
@@ -17,6 +18,7 @@ function App() {
           <Route path="/" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/blog" element={<BlogPage />} />
+          <Route path="/blog/:slug" element={<BlogDetail />} />
         </Routes>
       </BrowserRouter>
     </>
